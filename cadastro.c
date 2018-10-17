3b321dae523ac9eb114292780fec1657fb08acc2
Pedro <phjfarias@hotmail.com>
2018-10-17 16:01:33 -0300

    versão funcionando com os espaços

--- cadastro.c
+++ cadastro.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 typedef struct ficha
 {
@@ -15,6 +14,7 @@ int main(void)
     FILE *arduino; // arquivo com lista de pessoas que sabem arduino
     FILE *mod3d;   // pessoas que sabem modelagem 3d
     FILE *ps;      // pessoas que sabem photoshop
+    FILE *carta; 
 
     Ficha *ficha;
     ficha = (Ficha *)malloc(sizeof(Ficha));
@@ -23,20 +23,46 @@ int main(void)
     int quantidade; // Armazenara a quantidade de conhecimento do usuario
     char nome[24];  // Váriavel para armazenar o nome dos usuarios
     char descricao[1023];
-    printf("passei das declarações de variaveis\n"); 
+    char letra;
+    printf("passei das declarações de variaveis\n");
 
     // programa vai tentar procurar os arquivos, se não existir irá criar novos
     arduino = fopen("arduino.txt", "a");
     ps = fopen("photoshop.txt", "a");
     mod3d = fopen("modelagem.txt", "a");
+    carta = fopen("carta.txt", "a");
     printf("passei dos arquivos\n");
 
     // input de dados
     printf("Digite o seu nome: ");
-    scanf("%s", nome);
+    for (int i = 0; i < sizeof(nome); i++)
+    {
+        scanf("%c", &letra);
+        if (letra == '\n')
+        {
+            break;
+        }
+        else
+        {
+            nome[i] = letra;
+        }
+    }
     printf("Digite uma descricao: ");
-    scanf("%s", descricao);
-    
+    for (int i = 0; i < sizeof(descricao); i++)
+    {
+        scanf("%c", &letra);
+        if (letra == '\n')
+        {
+            break;
+        }
+        else
+        {
+            descricao[i] = letra;
+        }
+    }
+
+    fprintf(carta, "%s;%s\n", nome, descricao);
+
     // input arduino
     printf("Voce sabe arduino?(digite 1 para sim ou 0 para nao): ");
     scanf("%d", &check);
@@ -45,7 +71,7 @@ int main(void)
     {
         printf("Numa escala de 0 a 5 o quanto você sabe: ");
         scanf("%d", &quantidade);
-        fprintf(arduino, "%s;%d", nome, quantidade);
+        fprintf(arduino, "%s;%d\n", nome, quantidade);
     }
     else if (check != 1 && check != 0)
     {
@@ -59,7 +85,7 @@ int main(void)
     {
         printf("Numa escala de 0 a 5 o quanto você sabe: ");
         scanf("%d", &quantidade);
-        fprintf(ps, "%s;%d", nome, quantidade);
+        fprintf(ps, "%s;%d\n", nome, quantidade);
     }
     else if (check != 1 && check != 0)
     {
@@ -73,7 +99,7 @@ int main(void)
     {
         printf("Numa escala de 0 a 5 o quanto você sabe: ");
         scanf("%d", &quantidade);
-        fprintf(mod3d, "%s;%d", nome, quantidade);
+        fprintf(mod3d, "%s;%d\n", nome, quantidade);
     }
     else if (check != 1 && check != 0)
     {
@@ -82,3 +108,14 @@ int main(void)
 
     return 0;
 }
+
+/* 
+for(i = 0; sizeof(array); i++) {
+    scanf("%c", &char);
+    if(char == '\n') {
+        break;
+    }
+    else{
+        array[i] = char;
+    }
+}*/
