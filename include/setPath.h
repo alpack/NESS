9dcc4487c11926ddcd596337daf3a224ccfbee0a
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-10-25 08:33:24 -0300

    added setPath.h

---
+++ include/setPath.h
@@ -0,0 +1,16 @@
+/*
+ * 
+ * 
+ * 
+ */
+
+#ifndef SETPATH_H
+    #define SETPATH_H
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+
+    char *setPath(char *, char *, char *);
+
+    #endif
