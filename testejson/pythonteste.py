eb737edfb829afd7e1dfa58a4c88984f6c798058
Arthur O. Reis <aor@cesar.school>
2018-11-26 21:40:56 -0300

    needs more tests

--- testejson/pythonteste.py
+++ testejson/pythonteste.py
@@ -2,17 +2,15 @@ import json
 
 nomedoplayer = input(str('nome do player: '))
 
-# with open('newfile.json', 'a') as file:
-#     kleber = json.load(file)
-#     kleber['Player'] = nomedoplayer
-#     file.seek(0)
-#     json.dump(kleber, file, indent = 4)
-#     file.truncate()
-#     file.close()
+a = {
+    'id': nomedoplayer,
+    'Habilidades': {
+        'arduino': 5,
+    }
+}
 
-with open('newfile.json') as file:
-    data = file.read()
-arquivo = json.loads(data)
-arquivo["Player"] = nomedoplayer
+
 with open('newfile.json', 'a') as file:
-    json.dump(arquivo, file, indent = 4, sort_keys = True)
+    file.write(json.dumps(a, indent=4))
+
+file.close()
