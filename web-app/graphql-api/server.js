0c8de666166c473fd01a57806c7967e7eacf8b27
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-11-28 15:33:22 -0300

    graphql api

---
+++ web-app/graphql-api/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+import morgan from 'morgan'
+import { GraphQLServer } from 'graphql-yoga'
+import { resolvers } from './graphql-utils'
+import { getStaticFiles } from './helpers/utils'
+
+const server = new GraphQLServer({
+    typeDefs: './graphql-utils/schema/schema.graphql', 
+    resolvers, 
+    context: req => ({
+        ...req,
+        key: process.env.API_KEY
+    }),
+})
+
+const options = {
+    port: 8001,
+    endpoint: '/graphql',
+    subscriptions: '/subscriptions',
+    playground: '/playground',
+}
+if (process.env.DEV) {
+    require("nodejs-dashboard")
+    server.use(morgan('dev'))
+}
+
+server.use('*', (req, res, next) => {
+    if (req.method === 'GET' && req.originalUrl !== '/playground') {
+        return res.redirect('/playground')
+    }
+    next()
+})
+
+server.start(options, ({ port }) => console.log(`> Server started, listening on port ${port} for incoming requests.`) || getStaticFiles())
