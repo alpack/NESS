ec034527166e7584b2927a01a63612b87af76399
Pedro <phjfarias@hotmail.com>
2018-12-04 14:41:47 -0300

    front v1

--- web-app/app/src/App.js
+++ web-app/app/src/App.js
@@ -12,6 +12,7 @@
 
 import React, { Component } from 'react';
 import './App.css';
+import image from './img/Background_NESS.jpg';
 
 import aws_exports from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
@@ -21,17 +22,18 @@ import { Grid } from 'semantic-ui-react';
 import HerosListLoader from './components/allHeros';
 
 Amplify.configure(aws_exports);
-
 class App extends Component {
-  render() {
-    return (
-        <Grid padded>
-            <Grid.Column>
-                <HerosListLoader />
-            </Grid.Column>
-        </Grid>
-    );
-  }
+	render() {
+		return (
+			<div className="App-background">
+				<Grid padded>
+					<Grid.Column>
+						<HerosListLoader/>
+					</Grid.Column>
+				</Grid>
+			</div>
+		);
+	}
 }
 
 export default withAuthenticator(App, { includeGreetings: true });
